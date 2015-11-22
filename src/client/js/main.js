fe721145381c909e3027235c5fcb9e286caa7b7c
KyleRattet <krattet@gmail.com>
2015-11-22 11:03:43 -0700

    home page structure, wip

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -2,4 +2,9 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
 });
