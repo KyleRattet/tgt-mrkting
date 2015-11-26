cc8a30e45558e3feef8fb197a43827b69a14f535
KyleRattet <krattet@gmail.com>
2015-11-26 14:00:37 -0800

    discrete bar chart, wip

--- src/client/research/charts/national-discretebar-chart-directive.js
+++ src/client/research/charts/national-discretebar-chart-directive.js
@@ -27,7 +27,7 @@ app.directive('nationalDiscreteBarChartData', function () {
                     axisLabel: 'Percent of Population',
                     axisLabelDistance: -10
                 },
-                forceY: [0,30],
+                forceY: [0,25],
 
             },
             title: {
