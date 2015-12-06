fd1d68e15bdba694326dbb6c13dabee87fa89519
KyleRattet <krattet@gmail.com>
2015-12-06 15:31:15 -0700

    redir, wip

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -153,7 +153,7 @@ router.post('/github', function(req, res) {
   var params = {
     code: req.body.code,
     client_id: req.body.clientId,
-    redirect_uri: 'https://target-marketing.herokuapp.com/',
+    redirect_uri: window.location.origin,
     client_secret: config.GITHUB_SECRET
   };
 
