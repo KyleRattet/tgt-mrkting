2ca009daa98e5a764e79f0c6f54dfba7ed4f548d
KyleRattet <krattet@gmail.com>
2015-12-06 13:44:58 -0700

    updated routes

--- src/server/routes/query.js
+++ src/server/routes/query.js
@@ -8,42 +8,30 @@ var CENS_id = process.env.CENS_ID;
 
 router.get('/census/national', function(req, res, next) {
   var queryCodes = req.query.category;
-  console.log(queryCodes, 'query codes');
-
   var url = "http://api.census.gov/data/2014/acs1/profile?get=NAME,"+queryCodes+"&for=us:*&key="+CENS_id;
-  console.log(url, "url");
+
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
       console.log("Got error: " + e.message);
     });
-
 });
 
 router.get('/bea/popular', function(req, res, next) {
   var queryCodes = req.query.category;
-  console.log(queryCodes, "req query")
   var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode='+queryCodes+'&Year=2014&GeoFips=STATE&ResultFormat=json';
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
@@ -52,42 +40,29 @@ router.get('/bea/popular', function(req, res, next) {
 });
 
 router.get('/census/popular', function(req, res, next) {
-  console.log(req.query, "state req query server side");
-
   var queryCodes = req.query.category;
-  // var state = req.query.state;
   var url = "http://api.census.gov/data/2014/acs1/profile?get=NAME,"+queryCodes+"&for=state&key="+CENS_id;
-  console.log(url, "url state")
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
       console.log("Got error: " + e.message);
     });
-
 });
 
 router.get('/bea/gdp', function(req, res, next) {
   var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=GDP_SP&Year=2014&GeoFips=STATE&ResultFormat=json';
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
@@ -99,14 +74,10 @@ router.get('/bea/personal-income', function(req, res, next) {
   var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=PCPI_SI&Year=2014&GeoFips=STATE&ResultFormat=json';
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
@@ -118,14 +89,10 @@ router.get('/bea/population', function(req, res, next) {
   var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=POP_SI&Year=2014&GeoFips=STATE&ResultFormat=json';
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
@@ -137,14 +104,10 @@ router.get('/bea/job', function(req, res, next) {
   var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=EMP000_SI&Year=2014&GeoFips=STATE&ResultFormat=json';
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
@@ -153,63 +116,35 @@ router.get('/bea/job', function(req, res, next) {
 });
 
 router.get('/census/state', function(req, res, next) {
-  console.log(req.query, "state req query server side");
-
   var queryCodes = req.query.category;
   var state = req.query.state;
   var url = "http://api.census.gov/data/2013/acs1/profile?get=NAME,"+queryCodes+"&for=state:"+state+"&key="+CENS_id;
-  console.log(url, "url state")
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
       console.log("Got error: " + e.message);
     });
-
 });
 
 router.get('/census/county', function(req, res, next) {
-  console.log(req.query, "req  county query server side");
-
-  ///function to build up url
-  // var queryBuild = ifClickedCounty(req.query);
-  // console.log(queryBuild, "query string builder");
-  // var state = queryBuild[0];
-  // var county = queryBuild[1];
-  // var queryCodes = queryBuild[2];
-  // console.log(state, "state");
-  // console.log(queryCodes, "query codes");
-
-  // var url = "http://api.census.gov/data/2013/acs1/profile?get=NAME,"+queryCodes+"&for=county:"+county+"&in=state:"+state+"&key="+CENS_id;
-
-
-  var url = 'http://api.census.gov/data/2013/acs1/profile?get=NAME,DP05_0004PE,DP05_0005PE,DP05_0006PE,DP05_0007PE,DP05_0008PE,DP05_0009PE,DP05_0010PE,DP05_0011PE,DP05_0012PE,DP05_0013PE,DP05_0014PE,DP05_0015PE,DP05_0016PE,DP05_0001PE&for=county:031&in=state:08&key='+CENS_id;
-  console.log(url, "url");
+  var url = 'http://api.census.gov/data/2013/acs1/profile?get=NAME,"DP05_0004PE,DP05_0005PE,DP05_0006PE,DP05_0007PE,DP05_0008PE,DP05_0009PE,DP05_0010PE,DP05_0011PE,DP05_0012PE,DP05_0013PE,DP05_0014PE,DP05_0015PE,DP05_0016PE,DP05_0001PE"&for=county:031&in=state:08&key='+CENS_id;
   http.get(url, function(response) {
       var body = '';
-
       response.on('data', function(chunk) {
-
         body += chunk;
       });
-
       response.on('end', function() {
-
         res.send(JSON.parse(body));
       });
     }).on('error', function(e) {
       console.log("Got error: " + e.message);
     });
-
 });
 
 
--- test/test.user.js
+++ test/test.user.js
@@ -3,212 +3,87 @@ process.env.NODE_ENV = 'test';
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var mongoose = require('mongoose');
-// var http = require('http');
-
 var server = require('../src/server/app');
 var User = require('../src/server/models/user');
 var BEA_id = process.env.BEA_ID;
 var CENS_id = process.env.CENS_ID;
-
 var should = chai.should();
 chai.use(chaiHttp);
 
 
-
-
-xdescribe('Testing Main User Routes', function() {
-
-  User.collection.drop();
-
-  beforeEach(function(done){
-    var newUser = new User({
-      name:  "John Doe",
-      username: "John_Doe",
-      email: 'jdoe@test.com',
-      password: 'test',
-      githubProfileID:'JohnDoe',
-      googleProfileID: 'JonathonDoe',
-      queries: [ {
-        name: 'Test1',
-        url: 'http://testqueryforTest1'
-        },
-        {
-        name: 'Test2',
-        url: 'http://testqueryforTest2'
-        },
-        {
-        name: 'Test3',
-        url: 'http://testqueryforTest3'
-        }]
-    });
-    newUser.save(function(err) {
-      done();
-    });
-  });
-  afterEach(function(done){
-    User.collection.drop();
-    done();
-  });
-
-  it('should list ALL users', function(done) {
-    chai.request(server)
-      .get('/users')
-      .end(function(err, res){
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('array');
-        res.body[0].should.have.property('_id');
-        res.body[0].should.have.property('name');
-        res.body[0].should.have.property('username');
-        res.body[0].should.have.property('githubProfileID');
-        res.body[0].should.have.property('googleProfileID');
-        res.body[0].should.have.property('queries');
-        res.body[0].name.should.equal('John Doe');
-        res.body[0].email.should.equal('jdoe@test.com');
-        res.body[0].googleProfileID.should.equal('JonathonDoe');
-        res.body[0].username.should.equal('John_Doe');
-        res.body[0].queries[0].should.be.a('object');
-        res.body[0].queries[0].should.have.property('name');
-        res.body[0].queries[0].should.have.property('url');
-        res.body[0].queries[0].name.should.equal('Test1');
-        res.body[0].queries[0].url.should.equal('http://testqueryforTest1');
-        res.body[0].queries[2].url.should.equal('http://testqueryforTest3');
-        done();
-      });
-  });
-
-  it('should list one user on /<id> GET', function(done) {
-    var newUser = new User({
-      name:  "Jane Doe",
-      username: "Jane_Doe",
-      email: 'janedoe@test.com',
-      password: 'testjane',
-      githubProfileID:'JaneDoe',
-      googleProfileID: 'JaneDoe',
-      queries: [ {
-        name: 'TestJane1',
-        url: 'http://testqueryforTest1Jane'
-        },
-        {
-        name: 'TestJane2',
-        url: 'http://testqueryforTest2Jane'
-        },
-        {
-        name: 'TestJane3',
-        url: 'http://testqueryforTest3Jane'
-        }]
-    });
-    newUser.save(function(err, data) {
-      chai.request(server)
-        .get('/users/' + data.id)
-        .end(function(err, res) {
-          res.should.have.status(200);
-          res.should.be.json;
-          console.log(res.body);
-          res.body.should.be.a('object');
-          res.body.should.have.property('_id');
-          res.body.should.have.property('name');
-          res.body.should.have.property('username');
-          res.body.should.have.property('githubProfileID');
-          res.body.should.have.property('googleProfileID');
-          res.body.should.have.property('queries');
-          res.body.name.should.equal('Jane Doe');
-          res.body.email.should.equal('janedoe@test.com');
-          res.body.googleProfileID.should.equal('JaneDoe');
-          res.body.username.should.equal('Jane_Doe');
-          res.body.queries.should.be.a('array');
-          res.body.queries[0].should.have.property('name');
-          res.body.queries[0].should.have.property('url');
-          res.body.queries[0].name.should.equal('TestJane1');
-          res.body.queries[0].url.should.equal('http://testqueryforTest1Jane');
-          res.body.queries[2].url.should.equal('http://testqueryforTest3Jane');
-          done();
-        });
-    });
-  });
-
-  it("should update a query for a user on /user/id/addquery PUT", function(done){
-  chai.request(server)
-    .get('/users')
-    .end(function(err, res, data){
-      chai.request(server)
-        .put('/users/addquery')
-        .send({
-          'name': 'Test Query 14',
-          'url': "http://testquery1"
-          })
-        .end(function(error, response){
-          console.log(response.body)
-          response.should.have.status(200);
-          response.should.be.json;
-
-          // response.body.should.be.a('object');
-          // response.body.should.have.property('UPDATED');
-          // response.body.UPDATED.should.be.a('object');
-          // response.body.UPDATED.should.have.property('name');
-          // response.body.UPDATED.should.have.property('_id');
-          // response.body.UPDATED.name.should.equal('Google Inc.');
-          // response.body.UPDATED.shares.should.equal(200);
-          // response.body.UPDATED.costBasis.should.equal(670);
-          done();
-      });
-    });
-  });
-
-
-
-});
-
 describe('Testing Query Routes', function() {
-
-  xit('should list national data on age demographic', function(done) {
-
-    chai.request(server)
-      .get('/query/census/national')
-      .end(function(err, res){
-        res.should.have.status(200);
-        res.should.be.json;
-        // res.body.should.be.a('array');
-        // res.body[0][0].should.equal('NAME');
-        // res.body[0][1].should.equal('DP05_0004PE');
-        // res.body[0][2].should.equal('DP05_0005PE');
-        // res.body[1][0].should.equal('United States');
-        // res.body[1][1].should.equal('6.3');
-        done();
-      });
-  });
-
-  xit('should list the state of colorado data on age demographic', function(done) {
-    chai.request(server)
-      .get('/query/census/state')
-      .end(function(err, res){
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body[0][0].should.equal('NAME');
-        res.body[0][1].should.equal('DP05_0004PE');
-        res.body[0][2].should.equal('DP05_0005PE');
-        res.body[1][0].should.equal('Colorado');
-        res.body[1][1].should.equal('6.3');
-        res.body[1][3].should.equal('6.8');
-        done();
-      });
-  });
-
 
   it('should list denver county age demographic', function(done) {
     chai.request(server)
       .get('/query/census/county')
       .end(function(err, res){
         res.should.have.status(200);
-        console.log(res.body);
         res.should.be.json;
         res.body.should.be.a('array');
-        res.body[0][0].should.equal('NAME');
-        res.body[0][1].should.equal('DP05_0004PE');
-        res.body[0][2].should.equal('DP05_0005PE');
-        res.body[1][0].should.equal('Denver County, Colorado');
-        res.body[1][1].should.equal('7.0');
-        res.body[1][3].should.equal('5.4');
+        res.body[0][0].should.equal('state');
+        res.body[0][1].should.equal('county');
+        res.body[1][0].should.equal('08');
+        res.body[1][1].should.equal('031');
+        done();
+      });
+  });
+
+  it('should get bea job data', function(done) {
+    var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=EMP000_SI&Year=2014&GeoFips=STATE&ResultFormat=json'
+    chai.request(server)
+      .get('/query/bea/job')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('BEAAPI');
+        res.body.BEAAPI.Results.should.have.property('Data');
+        res.body.BEAAPI.Results.Statistic.should.equal('Total employment');
+        done();
+      });
+  });
+
+  it('should get bea population data', function(done) {
+    var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=POP_SI&Year=2014&GeoFips=STATE&ResultFormat=json';
+    chai.request(server)
+      .get('/query/bea/population')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('BEAAPI');
+        res.body.BEAAPI.Results.should.have.property('Data');
+        res.body.BEAAPI.Results.Statistic.should.equal('Population');
+        done();
+      });
+  });
+
+  it('should get bea personal income data', function(done) {
+    var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=PCPI_SI&Year=2014&GeoFips=STATE&ResultFormat=json';
+    chai.request(server)
+      .get('/query/bea/personal-income')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('BEAAPI');
+        res.body.BEAAPI.Results.should.have.property('Data');
+        res.body.BEAAPI.Results.Statistic.should.equal('Per capita personal income');
+        done();
+      });
+  });
+
+  it('should get bea gdp', function(done) {
+    var url = 'http://bea.gov/api/data/?UserID='+BEA_id+'-Key&method=GetData&datasetname=RegionalData&KeyCode=GDP_SP&Year=2014&GeoFips=STATE&ResultFormat=json';
+    chai.request(server)
+      .get('/query/bea/gdp')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('BEAAPI');
+        res.body.BEAAPI.Results.should.have.property('Data');
+        res.body.BEAAPI.Results.Statistic.should.equal('GDP in current dollars');
         done();
       });
   });
